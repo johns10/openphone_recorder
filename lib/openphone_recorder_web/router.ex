7fb11ef2a79c49df8667eeb0c08c7a465ddb45c4
John Davenport <johns10@gmail.com>
2023-04-12 22:01:51 -0400

    Fix session?

--- lib/openphone_recorder_web/router.ex
+++ lib/openphone_recorder_web/router.ex
@@ -7,6 +7,7 @@ defmodule OpenphoneRecorderWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, {OpenphoneRecorderWeb.Layouts, :root}
+    plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
