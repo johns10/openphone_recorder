85b1b5ddb5a80205d334a7f59e6aaac9282dd7c7
John Davenport <johns10@gmail.com>
2023-05-14 13:30:56 -0400

    summary query

--- lib/openphone_recorder/conversations.ex
+++ lib/openphone_recorder/conversations.ex
@@ -12,6 +12,21 @@ defmodule OpenphoneRecorder.Conversations do
     |> Repo.all()
   end
 
+  def list_conversation_summary do
+    from(c in Conversation)
+    |> join(:left, [c], p in assoc(c, :participants), as: :participants)
+    |> join(:left, [participants: p], pn in assoc(p, :phone_number), as: :phone_number)
+    |> join(:left, [phone_number: pn], c in assoc(pn, :contact_phone_numbers), as: :contact_phone_numbers)
+    |> join(:left, [contact_phone_numbers: cpn], c in assoc(cpn, :contact), as: :contacts)
+    |> order_by([contacts: c], [desc: fragment("CASE 'relationship' when 'primary' then 1 when 'internal' then 2 when 'external' then 3 end")])
+    |> order_by([contact_phone_numbers: cpn], [desc: fragment("CASE when 'contact_id' is NULL then 1 when 'contact_id' is not null then 2 end")])
+    |> preload(
+      [participants: p, phone_number: pn, contacts: c],
+      participants: {p, phone_number: {pn, contacts: c}}
+    )
+    |> Repo.all()
+  end
+
   def get_conversation!(id), do: Repo.get!(Conversation, id)
 
   def create_conversation(attrs \\ %{}) do
--- test/openphone_recorder/conversations_test.exs
+++ test/openphone_recorder/conversations_test.exs
@@ -7,12 +7,48 @@ defmodule OpenphoneRecorder.ConversationsTest do
     alias OpenphoneRecorder.Conversations.Conversation
 
     import OpenphoneRecorder.ConversationsFixtures
+    import OpenphoneRecorder.ParticipantsFixtures
+    import OpenphoneRecorder.PhoneNumbersFixtures
+    import OpenphoneRecorder.ContactsFixtures
+    import OpenphoneRecorder.ContactPhoneNumbersFixtures
 
     @invalid_attrs %{}
 
     test "list_conversations/0 returns all conversations" do
       conversation = conversation_fixture()
       assert Conversations.list_conversations() == [conversation]
+    end
+
+    test "list_conversation_summary works" do
+      con = conversation_fixture()
+      pn = phone_number_fixture()
+      pn2 = phone_number_fixture()
+      participant_fixture(%{conversation_id: con.id, phone_number_id: pn.id})
+      participant_fixture(%{conversation_id: con.id, phone_number_id: pn2.id})
+      c2 = contact_fixture(%{relationship: :external})
+      c = contact_fixture(%{relationship: :primary})
+      contact_phone_number_fixture(%{contact_id: c.id, phone_number_id: pn.id})
+      contact_phone_number_fixture(%{contact_id: c2.id, phone_number_id: pn2.id})
+      pn_id = pn.id
+      pn2_id = pn2.id
+
+      [conversation] = Conversations.list_conversation_summary()
+      assert [%{phone_number: %{id: ^pn_id}}, %{phone_number: %{id: ^pn2_id}}] = conversation.participants
+    end
+
+    test "list_conversation_summary orders participants with contacts first" do
+      con = conversation_fixture()
+      pn = phone_number_fixture()
+      pn2 = phone_number_fixture()
+      participant_fixture(%{conversation_id: con.id, phone_number_id: pn.id})
+      participant_fixture(%{conversation_id: con.id, phone_number_id: pn2.id})
+      c = contact_fixture(%{relationship: :primary})
+      contact_phone_number_fixture(%{contact_id: c.id, phone_number_id: pn.id})
+      pn_id = pn.id
+      pn2_id = pn2.id
+
+      [conversation] = Conversations.list_conversation_summary()
+      assert [%{phone_number: %{id: ^pn_id}}, %{phone_number: %{id: ^pn2_id}}] = conversation.participants
     end
 
     test "get_conversation!/1 returns the conversation with given id" do
