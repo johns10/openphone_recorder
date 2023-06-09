aa94345a29bfeceeee6a1dce6181a8cedd0d5879
John Davenport <johns10@gmail.com>
2023-06-09 08:26:28 -0400

    Fix type

--- lib/openphone_recorder/statements/chunker/behaviour.ex
+++ lib/openphone_recorder/statements/chunker/behaviour.ex
@@ -1,5 +1,5 @@
 defmodule OpenphoneRecorder.Statements.Chunker.Behaviour do
   @moduledoc false
-  @callback prompt_fun(atom(), binary(), keyword()) :: binary()
+  @callback prompt(atom(), binary(), keyword()) :: binary()
   @callback prompt_count(atom(), keyword()) :: integer
 end
