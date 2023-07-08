d383f8e6e2ed26291020cfe03f26ddbc930f19be
John Davenport <johns10@gmail.com>
2023-07-08 11:55:36 -0400

    Token count prompts

--- lib/openphone_recorder/chunker/token_count.ex
+++ lib/openphone_recorder/chunker/token_count.ex
@@ -58,8 +58,29 @@ defmodule OpenphoneRecorder.Chunker.TokenCount do
   end
 
   @impl true
-  def prompt_count(_opts), do: 0
+  def prompt_count(_opts) do
+    prompt_text("", 100)
+    |> Tokens.count()
+  end
 
   @impl true
-  def prompt(_, _), do: ""
+  def prompt(text, opts) do
+    max_text_output = Tokens.max_text_output_count(opts)
+    prompt_text(text, max_text_output)
+  end
+
+  def prompt_text(text, max_text_output) do
+    """
+    Write a summary of the following conversation.
+    Remove any irrelevant information and filler words.
+    The summary should be no longer than #{max_text_output} words.
+    \"\"\"#{text}\"\"\"
+
+    Return your answer in the following format:
+    AI can make humans more productive by automating many repetitive processes.
+    Keeping a clean and tidy home can improve your health by reducing the dust particles you breathe, and reducing your exposure to harmful bacteria.
+
+    SUMMARY:
+    """
+  end
 end
