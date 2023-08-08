e4e21f2990bae0b8f1dd4a12d1e9c40a44fa653b
John Davenport <johns10@gmail.com>
2023-08-08 08:23:37 -0700

    merge

--- test/discussit_web/live/index_test.exs
+++ test/discussit_web/live/index_test.exs
@@ -78,5 +78,34 @@ defmodule DiscussitWeb.IndexLive.IndexTest do
 
       assert html =~ to_string(pn.value)
     end
+
+    test "updates a contact when participant contact picked", %{
+      conn: conn,
+      phone_number: phone_number,
+      conversation: conversation
+    } do
+      contact_1 = Discussit.ContactsFixtures.contact_fixture()
+      contact_2 = Discussit.ContactsFixtures.contact_fixture()
+
+      Discussit.ContactPhoneNumbersFixtures.contact_phone_number_fixture(%{
+        phone_number_id: phone_number.id,
+        contact_id: contact_1.id
+      })
+
+      Discussit.ContactPhoneNumbersFixtures.contact_phone_number_fixture(%{
+        phone_number_id: phone_number.id,
+        contact_id: contact_2.id
+      })
+
+      {:ok, _index_live, _html} = live(conn, ~p"/conversations/#{conversation}")
+
+      # assert index_live
+      #        |> element("a#participant-dropdown-toggle-#{participant.id}")
+      #        |> render_click() =~ contact_1.first_name
+
+      # assert index_live
+      #        |> element("a#participant-contact-option-#{contact_2.id}")
+      #        |> render_click() =~ contact_2.first_name
+    end
   end
 end
