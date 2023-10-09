a1fc9108aadc9755b79480327b053a8d72176818
John Davenport <johns10@gmail.com>
2023-10-09 08:46:54 +0200

    Add stopwords and augment queries

---
+++ priv/repo/migrations/20231006083420_add_all_stopwords.exs
@@ -0,0 +1,9 @@
+defmodule Discussit.Repo.Migrations.AddAllStopwords do
+  use Ecto.Migration
+
+  def change do
+    alter table :statements do
+      add :all_stopwords, :boolean
+    end
+  end
+end
