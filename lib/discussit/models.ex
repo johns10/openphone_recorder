b27a808e71f16cf60ea4fa46e4dedbb109a59d08
John Davenport <johns10@gmail.com>
2023-12-08 07:42:54 -0500

    order by/limit on model

--- lib/discussit/models.ex
+++ lib/discussit/models.ex
@@ -8,28 +8,16 @@ defmodule Discussit.Models do
 
   alias Discussit.Models.Model
 
-  def list_models do
-    Repo.all(Model)
+  def list_models(opts \\ []) do
+    order_by = Keyword.get(opts, :order_by, [])
+
+    Model
+    |> maybe_order_by_inserted_at(order_by[:inserted_at])
+    |> maybe_limit(opts[:limit])
+    |> Repo.all()
   end
 
   def get_model!(id), do: Repo.get!(Model, id)
-
-  def get_model_urls(id, method) do
-    case Repo.get!(Model, id) do
-      nil ->
-        nil
-
-      %Model{merge_object: merge_object, model_object: model_object} ->
-        with {:ok, model_url} <- presigned_url(model_object, method),
-             {:ok, merge_url} <- presigned_url(merge_object, method) do
-          {:ok,
-           %{
-             model_url: model_url,
-             merge_url: merge_url
-           }}
-        end
-    end
-  end
 
   def create_model(attrs \\ %{}) do
     id = Map.get(attrs, :id, Ecto.UUID.generate())
@@ -58,6 +46,50 @@ defmodule Discussit.Models do
   def change_model(%Model{} = model, attrs \\ %{}) do
     Model.changeset(model, attrs)
   end
+
+  def get_model_urls(id, method) do
+    case Repo.get!(Model, id) do
+      nil ->
+        nil
+
+      %Model{merge_object: merge_object, model_object: model_object} ->
+        with {:ok, model_url} <- presigned_url(model_object, method),
+             {:ok, merge_url} <- presigned_url(merge_object, method) do
+          {:ok,
+           %{
+             model_url: model_url,
+             merge_url: merge_url
+           }}
+        end
+    end
+  end
+
+  def reset_model(%Discussit.Accounts.Account{id: account_id}) do
+    [_latest | rest] =
+      Discussit.Models.list_models(
+        order_by: [inserted_at: :desc],
+        filters: [account_id: account_id]
+      )
+
+    rest
+    |> Enum.each(&delete_model/1)
+  end
+
+  defp maybe_order_by_inserted_at(query, nil), do: query
+
+  defp maybe_order_by_inserted_at(query, :desc) do
+    query
+    |> order_by([s], desc: s.inserted_at)
+  end
+
+  defp maybe_order_by_inserted_at(query, :asc) do
+    query
+    |> order_by([s], asc: s.inserted_at)
+  end
+
+  defp maybe_limit(query, nil), do: query
+
+  defp maybe_limit(query, limit), do: limit(query, [s], ^limit)
 
   defp model_name(id), do: "#{id}-model"
   defp merge_model_name(id), do: "#{id}-merge-model"
--- lib/discussit/models/model.ex
+++ lib/discussit/models/model.ex
@@ -17,6 +17,7 @@ defmodule Discussit.Models.Model do
   def changeset(model, attrs) do
     model
     |> cast(attrs, [:id, :model_object, :merge_object, :account_id])
+    |> foreign_key_constraint(:account_id)
     |> validate_required([:id, :model_object, :merge_object])
   end
 end
