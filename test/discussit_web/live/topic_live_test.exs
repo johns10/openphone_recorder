64f8c653868fd534445d0b91dad848b75b5b75a4
John Davenport <johns10@gmail.com>
2023-12-17 22:26:58 -0500

    Fix test

--- test/discussit_web/live/topic_live_test.exs
+++ test/discussit_web/live/topic_live_test.exs
@@ -66,7 +66,7 @@ defmodule DiscussitWeb.TopicLiveTest do
 
     test "trains a model", %{conn: conn, account: account} do
       model = model_fixture(%{account_id: account.id})
-      {:ok, index_live, _html} = live(conn, ~p"/topics/train")
+      {:ok, _index_live, _html} = live(conn, ~p"/topics/train")
 
       # assert index_live |> element("#topics-#{topic.id} a", "Delete") |> render_click()
       # refute has_element?(index_live, "#topics-#{topic.id}")
